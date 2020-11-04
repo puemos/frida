ba6a0d03ff775cc54092729c394bd4f5a592cbe3
Shy <puemos@gmail.com>
2020-11-04 18:43:49 +0100

    change(auth): complete redesign login/register

--- lib/frida_web/views/error_helpers.ex
+++ lib/frida_web/views/error_helpers.ex
@@ -11,7 +11,7 @@ defmodule FridaWeb.ErrorHelpers do
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
+        class: "text-red-700 text-xs mt-1",
         phx_feedback_for: input_id(form, field)
       )
     end)
